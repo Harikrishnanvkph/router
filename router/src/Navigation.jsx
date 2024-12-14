b31f1c5f30bcc8741e4294f8d258920adc446b1c
Harikrishnanvkph <harikrishnanvk0531@gmail.com>
2024-12-14 15:39:05 +0530

    router commit

---
+++ router/src/Navigation.jsx
@@ -0,0 +1,13 @@
+
+
+function Navigation() {
+    return <>
+        <ul className="navigation">
+            <li className="tabs">Price and Plan</li>
+            <li className="tabs">About Us</li>
+            <li className="tabs">Investors Relations</li>
+        </ul>
+    </>
+}
+
+export default Navigation;
